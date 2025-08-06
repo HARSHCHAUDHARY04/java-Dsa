c15056c7635da2a99aa410c666617a681666cbfe
Vedansh27-dev <vedansh.gla_cs.aiml24@gla.ac.in>
2025-08-06 17:14:22 +0530

    updates

---
+++ lockin.py
@@ -0,0 +1,12 @@
+def add_two_lists():
+    list1 = list(map(int, input("Enter list 1: ").split()))
+    list2 = list(map(int, input("Enter list 2: ").split()))
+    
+    result = []
+    
+    for i in range(len(list1)):
+        result.append(list1[i] + list2[i])
+        
+    return result
+
+print(add_two_lists())
