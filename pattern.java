c275a28099bb206e6eda5e95cce8b0e167efb71d
Harsh chaudhary <harshchikara408@gmail.com>
2025-08-07 14:21:35 +0530

    added inverted pyramid

--- pattern.java
+++ pattern.java
@@ -13,5 +13,19 @@ public class pattern{
             // Move to the next line
             System.out.println();
         }
+
+        // Inverted pyramid
+        for (int i = n - 1; i >= 1; i--) {  
+            // Print leading spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            // Move to the next line
+            System.out.println();
+        }   
     }
 }
