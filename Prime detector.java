1b5a8146b7f856257655274db8e56d9489ad021b
Vedansh27-dev <vedansh.gla_cs.aiml24@gla.ac.in>
2025-08-07 13:39:22 +0530

    primenumber

---
+++ Prime detector.java
@@ -0,0 +1,20 @@
+
+public class Prime {
+    public static void main(String[] args) {
+        int n = 50;
+        System.out.println("Prime numbers between 1 to "+n+" are: ");
+        for (int i = 2; i <= n; i++) {
+            boolean isPrime = true;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
+
